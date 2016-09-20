3bb2ea793e51a5be9ba06f87f5b0ad82d0b03974
laripk <larikirby@fastmail.fm>
2016-09-20 01:27:10 +0000

    version tweaks for merge

--- OAuth2/Properties/AssemblyInfo.cs
+++ OAuth2/Properties/AssemblyInfo.cs
@@ -1,7 +1,12 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("OAuth2")]
+[assembly: AssemblyCopyright("Copyright (c) Constantin Titarenko, Andrew Semack, Lari Kirby and others")]
+[assembly: AssemblyDescription(@"Allows you to perform user authentication via 
+Asana, DigitalOcean, Facebook, Fitbit, Foursquare, GitHub, Google, Instagram, LinkedIn, MailRu, Odnoklassniki, 
+Salesforce, Spotify, Todoist, Twitter, Uber, VK (Vkontakte), Windows Live, Xing, Yahoo, Yandex 
+just in two method calls.")]
 
-[assembly: AssemblyInformationalVersion("0.8.70-lari0009")]
-[assembly: AssemblyVersion("0.8.70")]
-[assembly: AssemblyFileVersion("0.8.70")]
+[assembly: AssemblyInformationalVersion("0.8.75-lari0000")]
+[assembly: AssemblyVersion("0.8.75")]
+[assembly: AssemblyFileVersion("0.8.75")]
